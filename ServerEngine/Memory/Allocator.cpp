a69376e7e2890d13fb8bce0764f60b13dd75d1ff
NanSu <nansu0425@gmail.com>
2025-04-14 23:12:34 +0900

    Introduce StompAllocator for memory management
    This commit replaces the existing BaseAllocator with a new StompAllocator.
    The memory allocation and deallocation macros in Macro.h are updated to use
    StompAllocator in debug mode. The implementation of StompAllocator::Alloc
    and StompAllocator::Free is added in Allocator.cpp, utilizing Windows API
    functions for memory management. The new StompAllocator class is defined in
    Allocator.h, including its methods and a private constant for page size,
    while retaining the original BaseAllocator class.

--- ServerEngine/Memory/Allocator.cpp
+++ ServerEngine/Memory/Allocator.cpp
@@ -11,3 +11,19 @@ void BaseAllocator::Free(void* memory)
 {
     ::free(memory);
 }
+
+void* StompAllocator::Alloc(UInt64 size)
+{
+    const UInt64 pageCount = (size + kPageSize - 1) / kPageSize;
+    const UInt64 offset = pageCount * kPageSize - size;
+    const UInt64 base = reinterpret_cast<UInt64>(::VirtualAlloc(nullptr, pageCount * kPageSize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE));
+
+    return reinterpret_cast<void*>(base + offset);
+}
+
+void StompAllocator::Free(void* memory)
+{
+    const UInt64 data = reinterpret_cast<UInt64>(memory);
+    const UInt64 base = data - (data % kPageSize);
+    ::VirtualFree(reinterpret_cast<void*>(base), 0, MEM_RELEASE);
+}
--- ServerEngine/Memory/Allocator.h
+++ ServerEngine/Memory/Allocator.h
@@ -8,3 +8,18 @@ public:
     static void*    Alloc(UInt64 size);
     static void     Free(void* memory);
 };
+
+/*
+ * 할당된 메모리     : [|   페이지|   ][    페이지    ]   ...   [    페이지    ]|
+ * 데이터 위치       : [|       [|                  데이터                   ]|
+ * 주소             :  |base    |base + offset                              |base + size
+ */
+class StompAllocator
+{
+public:
+    static void*    Alloc(UInt64 size);
+    static void     Free(void* memory);
+
+private:
+    static constexpr UInt64     kPageSize = 4096;
+};
